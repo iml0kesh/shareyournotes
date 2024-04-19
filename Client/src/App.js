0d1dbf2c3294a4136355cb3c57faf5d994070907
Lokesh Matta <iamlokeshofficial@gmail.com>
2024-04-19 16:41:34 +0530

    urls changes

--- Client/src/App.js
+++ Client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/shareyournotes" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/create" element={<CreateNote />} />
         <Route path="/edit/:id" element={<UpdateNote />} />
